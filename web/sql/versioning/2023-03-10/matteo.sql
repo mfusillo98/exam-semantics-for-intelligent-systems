77704ac596ae0f53c315ca16e13a0226c0bca439
Matteo Fusillo <58771053+mfusillo98@users.noreply.github.com>
2023-03-10 08:06:34 +0100

    + Versione richiesta x esame Antonio e Alberto

---
+++ web/sql/versioning/2023-03-10/matteo.sql
@@ -0,0 +1,1 @@
+alter table recipes add image_url varchar(255) default null after url;
--- web/test.php
+++ web/test.php
@@ -1,5 +1,23 @@
 <?php
 require_once __DIR__ . '/php/FuxFramework/bootstrap.php';
 
+set_time_limit(0);
 
-var_dump(send_email("mfusillo98@gmail.com","test","test",PROJECT_NAME));
+$recipes = \App\Models\RecipesModel::queryBuilder()->whereNull("image_url")->whereNotNull("static_score");
+$web = new \Spekulatius\PHPScraper\PHPScraper;
+$web->setConfig(['disable_ssl' => true]);
+$web->setConfig([
+    'agent' => 'Mozilla/5.0 (X11; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/107.0'
+]);
+echo "OK";
+foreach ($recipes as $r) {
+    $web->go($r['url']);
+    $image_url = ($web->openGraph['og:image'] ?? null) ?: ($web->twitterCard['twitter:image'] ?? null);
+    if ($image_url) {
+        \App\Models\RecipesModel::save([
+            "recipe_id" => $r["recipe_id"],
+            "image_url" => $image_url
+        ]);
+        print_r_pre($image_url);
+    }
+}
